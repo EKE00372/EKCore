69ec8470893e3e5e39f72193017403dda9f0c97f
EK <eke00372@gmail.com>
2018-09-04 19:53:39 +0800

    setaddons: add aurora

--- AddonSettings/SetAddons.lua
+++ AddonSettings/SetAddons.lua
@@ -11,6 +11,42 @@ local function SetCQL()
 	}
 end
 
+
+local function SetAurora()
+	if not IsAddOnLoaded("AuroraClassic") then return end
+	if AuroraConfig then table.wipe(AuroraConfig) end
+	AuroraConfig = {
+		["useButtonGradientColour"] = true,
+		["shadow"] = true,
+		["fontScale"] = 1,
+		["chatBubbles"] = true,
+		["bags"] = true,
+		["alpha"] = 0.7,
+		["reskinFont"] = true,
+		["buttonGradientColour"] = {
+			0.3, -- [1]
+			0.3, -- [2]
+			0.3, -- [3]
+			0.3, -- [4]
+		},
+		["loot"] = true,
+		["tooltips"] = true,
+		["useCustomColour"] = false,
+		["customColour"] = {
+			["b"] = 1,
+			["g"] = 1,
+			["r"] = 1,
+		},
+		["bubbleColor"] = false,
+		["buttonSolidColour"] = {
+			0.2, -- [1]
+			0.2, -- [2]
+			0.2, -- [3]
+			1, -- [4]
+		},
+	}
+end
+
 -- Litebag
 local function SetLiteBag()
 	if not IsAddOnLoaded("LiteBag") then return end
@@ -41,4 +77,5 @@ SetAddon:RegisterEvent("PLAYER_ENTERING_WORLD")
 SetAddon:SetScript("OnEvent", function()
 	SetLiteBag()
 	SetCQL()
+	SetAurora()
 end)
