c40b3cc4ad5fe08721ed77e713385d15e9012243
EK <eke00372@gmail.com>
2018-10-01 11:51:56 +0800

    tweaks: change gamma by mapid

--- Tweaks.lua
+++ Tweaks.lua
@@ -50,13 +50,13 @@ local function defaultsetting()
 
 	-- Font
 	-- 任務字體(影響全局字體)
-	QuestTitleFont:SetFont(STANDARD_TEXT_FONT, 18)				-- 標題
+	QuestTitleFont:SetFont(STANDARD_TEXT_FONT, 18)		-- 標題
 	QuestTitleFont:SetShadowOffset(0, 0)
-	QuestFont:SetFont(STANDARD_TEXT_FONT, 18)					-- 描述
+	QuestFont:SetFont(STANDARD_TEXT_FONT, 18)			-- 描述
 	QuestFont:SetShadowOffset(0, 0)
-	QuestFontNormalSmall:SetFont(STANDARD_TEXT_FONT, 18)		-- 目標
+	QuestFontNormalSmall:SetFont(STANDARD_TEXT_FONT, 18)-- 目標
 	QuestFontNormalSmall:SetShadowOffset(0, 0)
-	QuestFontHighlight:SetFont(STANDARD_TEXT_FONT, 18)			-- 內容
+	QuestFontHighlight:SetFont(STANDARD_TEXT_FONT, 18)	-- 內容
 	QuestFontHighlight:SetShadowOffset(0, 0)
 end 
 
@@ -67,6 +67,49 @@ local function eventHandler(self, event, ...)
 	defaultsetting() 
 end 
 DFS:SetScript("OnEvent", eventHandler)
+
+-- [[ 根據地點調整亮度 ]] --
+
+local ID = {
+	--[85] = true,	-- 奧格瑪
+	[86] = true,	-- 奧格瑪暗影裂谷
+	
+	[1015] = true,	-- 威奎斯特莊園 / Waycrest Manor - The Grand Foyer 
+	[1016] = true,	-- 威奎斯特莊園 / Waycrest Manor - Upstairs
+	[1017] = true,	-- 威奎斯特莊園 / Waycrest Manor - The Cellar
+	[1018] = true,	-- 威奎斯特莊園 / Waycrest Manor - Catacombs
+	
+	[974] = true,	-- 托達戈爾 / Tol Dagor
+	[975] = true,	-- 托達戈爾 / Tol Dagor - The Drain
+	[976] = true,	-- 托達戈爾 / Tol Dagor - The Brig
+	[977] = true,	-- 托達戈爾 / Tol Dagor - Detention Block
+	[978] = true,	-- 托達戈爾 / Tol Dagor - Officer Quarters
+	[979] = true,	-- 托達戈爾 / Tol Dagor - Overseer's Redoubt
+	[980] = true,	-- 托達戈爾 / Tol Dagor - Overseer's Summit
+
+	[1169] = true,	-- 托達戈爾野外 / Tol Dagor
+
+}
+
+local function changeGamma()
+	local MapId = C_Map.GetBestMapForUnit("player")
+	if MapId and ID[MapId] then
+		SetCVar("Gamma", 1.2)
+	else
+		SetCVar("Gamma", 1)
+	end
+end
+
+local CG = CreateFrame("Frame", "changeGamma")
+	CG:RegisterEvent("PLAYER_LOGIN")
+	CG:RegisterEvent("PLAYER_ENTERING_WORLD")
+	CG:RegisterEvent("ZONE_CHANGED")
+	CG:RegisterEvent("ZONE_CHANGED_INDOORS")
+	CG:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+local function eventHandler(self, event, ...)
+	changeGamma() 
+end 
+CG:SetScript("OnEvent", eventHandler)
 
 -- [[ 節日隨機自動排 ]] --
 
@@ -120,6 +163,7 @@ AutoScreenshot:Hide()
 AutoScreenshot:RegisterEvent("ACHIEVEMENT_EARNED")
 AutoScreenshot:RegisterEvent("PLAYER_DEAD")
 AutoScreenshot:RegisterEvent("PLAYER_LEVEL_UP")
+AutoScreenshot:RegisterEvent("CHALLENGE_MODE_COMPLETED") -- SCENARIO_COMPLETED/SCENARIO_CRITERIA_UPDATE/SCENARIO_UPDATE?
 
 AutoScreenshot:SetScript("OnUpdate", function(self, elapsed)
 	time = time + elapsed
