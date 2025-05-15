f8a9ed43695502dcae2d3cddec5d79b3279250fb
EK <eke00372@gmail.com>
2025-05-16 07:08:28 +0800

    Optimize CombatAlertSound

--- Combat/CombatAlertSound.lua
+++ Combat/CombatAlertSound.lua
@@ -1,68 +1,88 @@
--- BabyCombatMurloc! from Nynaeve: https://www.wowinterface.com/downloads/info21135-BabyCombatMurloc.html
+local addon, ns = ...
+local C, F, G, L = unpack(ns)
+local modf, GetTime = math.modf, GetTime
+local UnitIsDeadOrGhost, UnitHealth, UnitHealthMax = UnitIsDeadOrGhost, UnitHealth, UnitHealthMax
+local UnitPowerType, UnitPower, UnitPowerMax = UnitPowerType, UnitPower, UnitPowerMax
+local IsInInstance = IsInInstance
 
 -- [[ 團隊確認警示音 ]] --
 
-local ReadyCheckAlert = CreateFrame("Frame")
-ReadyCheckAlert:RegisterEvent("READY_CHECK")
-ReadyCheckAlert:SetScript("OnEvent", function()
-	PlaySound(8960, "master")	-- ReadyCheck
-end)
+do
+	local rcAlert = CreateFrame("Frame")
+	rcAlert:RegisterEvent("READY_CHECK")
+	rcAlert:SetScript("OnEvent", function()
+		PlaySound(8960, "Master")
+	end)
+end
 
 -- [[ 低血量警報 ]] --
 
-local last = 0
-local LowHP = CreateFrame("Frame")
-LowHP:RegisterUnitEvent("UNIT_HEALTH", "player")
-LowHP:SetScript("OnEvent", function() 
-	-- 死了不算
-	if UnitIsDeadOrGhost("player") then return end
-	-- 報警閾值
-    local lowHealth = UnitHealth("player") / UnitHealthMax("player") < 0.3
-	-- 限制時間間隔
-	local now = GetTime()
-    if now - last < 2 then return end
-	-- 警報聲
-	if lowHealth then
-		--PlaySoundFile("Sound\\Interface\\RaidWarning.ogg", "Master") 
-		PlaySoundFile("Interface\\Addons\\EKcore\\Media\\HealthWarning.ogg", "Master")
-		last = now 
-	end
-end)
+do
+	local lastHP = 0
+	local lowHP = CreateFrame("Frame")
+	lowHP:RegisterUnitEvent("UNIT_HEALTH", "player")
+	lowHP:SetScript("OnEvent", function()
+		-- 死了不報
+		if UnitIsDeadOrGhost("player") then return end
+		-- 警報閾值
+		local maxHealth = UnitHealthMax("player")
+		if not maxHealth or maxHealth == 0 then return end
+		local lowHealth = (UnitHealth("player") / maxHealth) < 0.3
+		-- 頻率限制
+		local now = modf(GetTime())
+		if now - lastHP < 1 then return end
+		-- 警報聲
+		if lowHealth then
+			PlaySoundFile(G.HealthWarning, "Master")
+			lastHP = now
+		end
+	end)
+end
+
+-- [[ 低法力警報 ]] --
+
+do
+	local lastMP = 0
+	local lowMP = CreateFrame("Frame")
+	lowMP:RegisterUnitEvent("UNIT_POWER_UPDATE", "player", 0)
+	lowMP:SetScript("OnEvent", function()
+		-- 死了不報
+		if UnitIsDeadOrGhost("player") then return end
+		-- 非法力不報
+		if UnitPowerType("player") ~= "MANA" then return end
+		-- 警報閾值
+		local maxMana = UnitPowerMax("player", 0)
+        if not maxMana or maxMana == 0 then return end
+		local lowMana = (UnitPower("player", 0) / maxMana) < 0.3
+		-- 頻率限制
+		local now = modf(GetTime())
+		if now - lastMP < 20 then return end
+		-- 警報聲
+		if lowMana then
+			PlaySoundFile(G.ManaWarning, "Master")
+			lastMP = now
+		end
+	end)
+end
 
 -- [[ 暫離狀態戰鬥警報 ]] --
 
-local AfkAggro = CreateFrame("Frame")
-AfkAggro:RegisterEvent("PLAYER_REGEN_DISABLED")
-AfkAggro:RegisterEvent("PLAYER_REGEN_ENABLED")
-AfkAggro:SetScript("OnEvent", function(self, event, ...)
-	--副本裡不算
-	local _, instanceType = IsInInstance()
-	if instanceType == "raid" then return end
-	if not IsChatAFK() then return end
-	--警報聲
-	if event == "PLAYER_REGEN_DISABLED" then	--進入戰鬥
-		PlaySoundFile("Sound\\Creature\\BabyMurloc\\BabyMurlocA.ogg", "Master")
-	elseif event == "PLAYER_REGEN_ENABLED" then	--離開戰鬥
-		StopMusic()
-	end
-end)
-
--- [[ 戰復警示音 ]] --
-
-local battlerez = {
-	[95750]  = true,
-	[20484]  = true,
-	[113269] = true,
-	[61999]  = true,
-	[126393] = true,
-}
-local BattleResAlert = CreateFrame("Frame")
-local function OnEvent(self, event, ...)
-	local _, subEvent, _, _, sourceName, _, _, _, destName, _, _, spellID  = CombatLogGetCurrentEventInfo()
-	if subEvent == "SPELL_CAST_SUCCESS" and destName == UnitName("player") and battlerez[spellID] then
-		--DEFAULT_CHAT_FRAME:AddMessage("已被"..sourceName.."戰復")
-		PlaySound(12889, "Master")	-- AlarmClockWarning3
-	end
+do
+	local afkAggro = CreateFrame("Frame")
+	afkAggro:RegisterEvent("PLAYER_REGEN_DISABLED")
+	afkAggro:RegisterEvent("PLAYER_REGEN_ENABLED")	
+	afkAggro:SetScript("OnEvent", function(self, event, ...)
+		-- 只在野外生效
+		local _, instanceType = IsInInstance()
+		if instanceType ~= "none" then return end
+		-- 只在暫離狀態啟用
+		if not IsChatAFK() then return end
+		-- 警報聲
+		if event == "PLAYER_REGEN_DISABLED" then	-- 進入戰鬥
+			--PlaySound(8475, "Master")
+			PlaySoundFile(544715, "Master")
+		elseif event == "PLAYER_REGEN_ENABLED" then	-- 離開戰鬥
+			StopMusic()
+		end
+	end)
 end
-BattleResAlert:SetScript("OnEvent", OnEvent)
-BattleResAlert:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
